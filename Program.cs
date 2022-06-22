9c847c701537b021a65d1a9371fc6f985ed6b93b
SebastianDevus <sebastiandevus@gmail.com>
2022-06-21 22:20:24 -0300

    Atividade Finalizada

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿int altura, largura, espacoVazioHorizontal, espacoVazioVertical;
+
+Console.Clear();
+Console.WriteLine("Insira as dimensões do retângulo.");
+
+Console.Write("Altura...:");
+altura = Convert.ToInt32(Console.ReadLine());
+Console.Write("Largura..:");
+largura = Convert.ToInt32(Console.ReadLine());
+
+espacoVazioHorizontal = largura - 2;
+espacoVazioVertical = altura - 2;
+
+for (int iTudo = 1; iTudo <= altura; iTudo++)
+{
+    if (iTudo == 1 || iTudo == altura)
+    {
+        for (int iLargura = 1; iLargura <= largura; iLargura++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.Write("*");
+
+        for (int iVazio = 1; iVazio <= espacoVazioHorizontal; iVazio++)
+        {
+            Console.Write(" ");
+        }
+
+        Console.Write("*");
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("\nSucesso!\n");
